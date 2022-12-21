12885e266bf7c1b0a3460f8cfa62e185a47084ef
RifadhRFD <rifadhrfd@gmail.com>
2022-12-21 20:22:26 +0530

    advise generator with api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [advise,setAdvise] = useState('');
+  
+  useEffect(() => {
+      fetch("https://api.adviceslip.com/advice")
+        .then(res => res.json())
+        .then(
+          (advise) => {
+            setAdvise(advise.slip.advice);
+          }
+        )
+  },[]);
+
+  let fetchNewAdvise = () => {
+    fetch("https://api.adviceslip.com/advice")
+        .then(res => res.json())
+        .then(
+          (advise) => {
+            setAdvise(advise.slip.advice);
+          }
+        )
+
+  }
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="App">
+      <div className="quote">  
+        <h2>{advise}</h2>
+      </div>
+      <button className="Btn-Advice" onClick={fetchNewAdvise} >Advice Me</button>
     </div>
   );
 }
